ddab424beebe5ffbe967f6d0e9871cbfd4799069
Ben Meier <benmeier@fastmail.com>
2015-08-02 20:01:38 +0200

    Added real vigenere crack

--- krypton/code/vigenere_cracker.py
+++ krypton/code/vigenere_cracker.py
@@ -1,44 +1,47 @@
 
 from itertools import combinations_with_replacement
-
 from code.ngram_scorer import NGramScorer
 
-ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+def _shift_c(c, distance):
+    return chr((((ord(c) - 65) + distance) % 26) + 65)
 
-def _apply_key_letter(c, kl):
-    a = ord(c) - 65
-    b = ord(kl) - 65
-    return chr(((a + b) % 26) + 65)
-
-
-def _apply_key(text, key):
-    new_text = ""
-    for i in xrange(0, len(text)):
-        new_text += _apply_key_letter(text[i], key[i % len(key)])
-    return new_text
+def _shift(s, distance):
+    return ''.join(map(lambda c: _shift_c(c, distance), s))
 
 
 def crack(text, key_length):
     text = text.replace(' ', '')
+    text_length = len(text)
+    sub_texts = [text[i::key_length] for i in xrange(0, key_length)]
 
-    scorer = NGramScorer('english_quadgrams.txt')
+    scorer = NGramScorer('english_monograms.txt')
 
-    best_key = None
-    best_score = -10**10
-    best_text = None
+    best_shifts = []
+    best_texts = []
 
-    i = 0
-    for current_key in combinations_with_replacement(ALPHABET, key_length):
+    for sub_text in sub_texts:
 
-        current_text = _apply_key(text, current_key)
-        current_score = scorer.score(current_text)
+        best_text = None
+        best_score = -10**10
+        best_shift = None
 
-        if current_score > best_score:
-            best_score = current_score
-            best_key = current_key
-            best_text = current_text
-            print i, best_key
-        i += 1
+        for i in xrange(26):
 
-    return best_text, best_key, best_score
+            current_text = _shift(sub_text, i)
+            current_score = scorer.score(current_text)
+
+            if current_score > best_score:
+                best_score = current_score
+                best_text = current_text
+                best_shift = i
+
+        best_shifts.append(best_shift)
+        best_texts.append(best_text)
+
+    real_best_text = ""
+    for i in xrange(0, text_length):
+        real_best_text += best_texts[i % key_length][i / key_length]
+
+    return real_best_text, best_shifts
+
